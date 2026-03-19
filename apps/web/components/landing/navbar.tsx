d2821cb8ee4c8becc67b86df37204b3136a3a0e5
Poul Poulsen <ppoulsen@grundfos.com>
2026-03-19 13:13:17 +0100

    chore: move changelog to footer, keep themes in navbar

--- apps/web/components/landing/navbar.tsx
+++ apps/web/components/landing/navbar.tsx
@@ -9,7 +9,6 @@ const navLinks = [
   { label: "Components", href: "/docs/components" },
   { label: "Themes", href: "/themes" },
   { label: "Docs", href: "/docs" },
-  { label: "Changelog", href: "/changelog" },
   { label: "GitHub", href: "https://github.com/iampoul/s0" },
 ]
 
